ddd4e5c86954e7c4413a23f7354b36b57c83e465
Lan <s35241607s@gmail.com>
2023-12-29 23:05:54 +0800

    加入專案檔案。

---
+++ GrpcClientApp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Grpc.Net.Client;
+using GrpcService;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7290");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = client.SayHello(new HelloRequest()
+{
+    Name = "Lan"
+});
+
+Console.WriteLine(reply.Message);
+
+Console.ReadLine();
